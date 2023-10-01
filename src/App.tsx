9edca04878ff6b3bfcb3761bd6e30a3a3d2847b3
ahita1 <ahitafani833@gmail.com>
2023-10-01 16:12:27 +0300

    build a responsive layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
 
   return (
-<div>
-<Button colorScheme='blue'>Button</Button>
-</div>
+    <>
+      <Grid templateAreas={{
+        base : `"nav " " main"`,
+        lg: `"nav nav" "aside main"`
+      }}>
+        <GridItem area='nav' bg='coral'>Nav</GridItem>
+       <Show above='lg'>
+       <GridItem area='aside' bg='gold'>Aside</GridItem>
+       </Show>
+        <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+      </Grid>
+    </>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
+import { HStack, Image } from '@chakra-ui/react'
+import logo from "../assets/logo.webp"
 const NavBar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div>
+            <HStack>
+                <Image src={logo} />
+            </HStack>
+        </div>
+    )
 }
-
 export default NavBar
