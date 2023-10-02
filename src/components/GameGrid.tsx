4c8886da3722ba3b3dcbbcd1b75187bdf6d99e16
ahita1 <ahitafani833@gmail.com>
2023-10-02 22:30:28 +0300

    Fetch The Genres haha

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,8 +3,6 @@ import useGames from "../hooks/useGames"
 import GameCards from "./GameCards"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
-
-
 const GameGrid = () => {
     const { games , error , Loading} = useGames()
     const Skeleton = [1,2,3,4,5,6,7,8,9,10,11,12]
