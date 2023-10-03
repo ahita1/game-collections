50e6a2a67cd357b11fbb06f4844a4f6fab28de47
ahita1 <ahitafani833@gmail.com>
2023-10-03 11:59:28 +0300

    Filtering Games  by Genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,17 +3,27 @@ import useGames from "../hooks/useGames"
 import GameCards from "./GameCards"
 import GameCardSkeleton from "./GameCardSkeleton"
 import GameCardContainer from "./GameCardContainer"
-const GameGrid = () => {
-    const { data , error , Loading} = useGames()
+import { Genres } from "../hooks/useGenres"
+
+interface Props{
+selectedGenre : Genres | null
+}
+
+
+const GameGrid = ({selectedGenre} : Props) => {
+    const { data , error , Loading} = useGames(selectedGenre)
     const Skeleton = [1,2,3,4,5,6,7,8,9,10,11,12]
   return (
   <>
   {error && <Text>{error}</Text>}
    <SimpleGrid columns={{sm : 1, md : 2, lg: 3, xl: 5}} padding={10} spacing={3}>
-    {Loading && Skeleton.map((skeleton) => <GameCardContainer><GameCardSkeleton key={skeleton}/></GameCardContainer>)}
+    {Loading && Skeleton.map((skeleton) => 
+    <GameCardContainer key={skeleton}>
+      <GameCardSkeleton />
+      </GameCardContainer>)}
     {data.map(game => 
-    <GameCardContainer>
-      <GameCards key={game.id} game={game}/> 
+    <GameCardContainer key={game.id}>
+      <GameCards  game={game}/> 
     </GameCardContainer>
     )}
    
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,14 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
-import useGenres from '../hooks/useGenres'
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
+import useGenres, { Genres } from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/Image-url'
 
-const GenreList = () => {
-const { data , Loading } = useGenres()
+interface Props{
+  onSelectGenre : (genre : Genres) => void
+}
 
+const GenreList = ({onSelectGenre} : Props) => {
+  const { data, Loading, error } = useGenres()
+  if (error) return null
 if(Loading) return <Spinner />
 
   return (
@@ -12,9 +16,10 @@ if(Loading) return <Spinner />
       {data.map(genre => <ListItem paddingY='5px' key={genre.id}>
         <HStack>
           <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-          <Text fontSize='lg'>{genre.name}
-          </Text>
-        </HStack>
+          <Button onClick={() => onSelectGenre(genre)} variant='link' fontSize='lg'>
+            {genre.name}
+          </Button >
+        </HStack> 
       </ListItem>)}
     </List>
   )
