15844a26f798d89f520e629f2badb3deb25ff7b7
ahita1 <ahitafani833@gmail.com>
2023-11-16 21:14:10 +0300

    Add new features

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { HStack, Image } from '@chakra-ui/react'
+import logo from "../assets/logo.webp"
+import ColorModeSwitch from './ColorModeSwitch'
+import SearchInput from './SearchInput'
+const NavBar = () => {
+    return (
+        <div>
+            <HStack justifyContent={'space-between'} padding='10px'>
+                <Image src={logo} boxSize='60px' />
+                <SearchInput />
+                <ColorModeSwitch/>
+            </HStack>
+        </div>
+    )
+}
+export default NavBar
